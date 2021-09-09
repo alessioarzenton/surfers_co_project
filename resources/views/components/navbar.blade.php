8ac98f507396708bde9f2f6c627f460e1e52f4fa
Alessio Arzenton <aless.arzenton@gmail.com>
2021-09-10 00:46:17 +0200

    header creata, passare alla card prodotto

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,20 @@
+<nav class="navbar navbar-light">
+    <div class="container-fluid">
+        <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 montserrat-bold navbar-brand fs-3" style="font-size:34px">
+            Surfers Co.
+        </a>
+      <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+        <li><a href="#" class="nav-link px-2 text-dark montserrat-bold" style="font-size:14px">BOARDS</a></li>
+        <li><a href="#" class="nav-link px-2 text-dark montserrat-bold" style="font-size:14px">ACCESSORIES</a></li>
+        <li><a href="#" class="nav-link px-2 text-dark montserrat-bold" style="font-size:14px">BLOG</a></li>
+        <li><a href="#" class="nav-link px-2 text-dark montserrat-bold" style="font-size:14px">TECHNOLOGY</a></li>
+        <li><a href="#" class="nav-link px-2 text-dark montserrat-bold" style="font-size:14px">TEAM</a></li>
+        <li><a href="#" class="nav-link px-2 text-dark montserrat-bold" style="font-size:14px">DEALERS</a></li>
+      </ul>
+      <div class="col-md-3 text-end">
+        <a href="" class="p-2" style="background-color: black; border-radius: 50%;"><i class="bi bi-facebook text-white"></i></a>
+        <a href="" class="p-2" style="background-color: black; border-radius: 50%;"><i class="bi bi-instagram text-white"></i></a>
+        <a href="" class="p-2" style="background-color: black; border-radius: 50%;"><i class="bi bi-envelope-fill text-white"></i></a>
+      </div>
+    </div>
+</nav>
